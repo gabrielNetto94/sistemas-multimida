ba66ff7a8e58bec94fdc106cfe665e2a586d83d9
unknown <silveranetto94@gmail.com>
2019-09-12 18:21:23 -0300

    exe1 openCV

---
+++ ProjetoOpenCV/Python/exe1.py
@@ -0,0 +1,7 @@
+from cv2 import cv2
+import numpy as np
+
+img = cv2.imread("windows.jpg")
+
+#cv2.imshow('img',img)
+cv2.waitKey(0)
--- ProjetoOpenCV/Python/teste.py
+++
@@ -1,5 +0,0 @@
-from cv2 import cv2
-import numpy
-img = numpy.zeros([320, 320], numpy.uint8)
-cv2.imshow("test", img)
-cv2.waitKey(0)
